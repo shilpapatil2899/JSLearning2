ebf874c73f0ca5354d80dc6eafb30c3d630eaac5
Shilpa Patil <123926545+shilpapatil2899@users.noreply.github.com>
2023-02-28 16:01:12 +0530

    funtionAssignment.js
    funtionAssignment.js

---
+++ 03_funtionAssig.js
@@ -0,0 +1,37 @@
+function display(){
+    console.log("1) My Hobbey is Learning New things");
+    console.log("___________");
+
+    console.log("My Fav Movie South");
+    console.log("___________");
+
+}
+display();
+
+function personalDetails(firstName,lastName,collegeName){
+    console.log(firstName,lastName,collegeName);
+    console.log("___________");
+
+}
+console.log("2)");
+personalDetails("shilpa","patil","sandip univarcity")
+
+ 
+function swapValues(value1,value2){
+    console.log(" before swaping",value1,value2);
+    var temp=value1;
+    value1=value2;
+    value2=temp;
+    console.log("after swaping",value1,value2);
+    console.log("___________");
+}
+console.log("3)");
+swapValues("Virat","Anushka");
+swapValues(1000,2000);
+
+function addThreeValues(value1,value2,value3){
+    console.log(value1+value2+value3);
+}
+console.log("4)");
+addThreeValues(10.23,600,40);
+addThreeValues("hello","good","morning");

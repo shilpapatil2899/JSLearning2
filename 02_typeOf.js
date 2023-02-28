7a942b3ce8aa1fef86aa671948174bb1f5acd7b5
Shilpa Patil <123926545+shilpapatil2899@users.noreply.github.com>
2023-02-28 14:49:42 +0530

    02_typeOf.js
    02_typeOf.js

---
+++ 02_typeOf.js
@@ -0,0 +1,23 @@
+var bankName
+console.log("type of blankName before intilizing:", bankName);
+bankName = "SBI bank"
+console.log("type of bankName after intilizing:", bankName)
+
+console.log("******************")
+var mark = "90%";
+console.log(`log ${mark} typOff: ${typeof (mark)}`);
+
+console.log("*****************************");
+var isWorking;
+console.log("the type of isWorking is before intalizing:", typeof(isWorking));
+isWorking = true;
+
+console.log("type of isWorking after initilizing:", typeof(isWorking));
+console.log("are you curruntly working:", isWorking);
+console.log("*****************************");
+
+var totalCount = "hundred and seven";
+console.log("the total count is :", totalCount, "and the type of totalCount is :", typeof(totalCount));
+totalCount = 107;
+
+console.log("after overring the total count the count is :", totalCount, "and the type is", typeof(totalCount));

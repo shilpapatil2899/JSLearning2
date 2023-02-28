1b127220d893d0c649ea4b3ef8c01d6909cca9ec
Shilpa Patil <123926545+shilpapatil2899@users.noreply.github.com>
2023-02-28 14:06:34 +0530

    01 varibleAsssign.js
    01 varibleAsssign.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,7 @@
 console.log("shilpa patil");
+var fullName=("shilpa patil")
+console.log(fullName);
+
+var city;//this is variable declration
+city="pune";//this is varible declration
+console.log(city);
---
+++ variableAssign.js
@@ -0,0 +1,22 @@
+var contry;
+console.log(contry);
+
+var myName;  //declaration
+myName="shilpa"; //initialise
+console.log(myName);
+
+var myFullName;
+myFullName="shilpa Liladhar Patil";
+console.log(myFullName);
+
+var myLove="i love only js";
+console.log(myLove);
+
+var firstName="shilpa"
+var lastName="patil"
+var myAge="23";
+var address="pune ";
+var pincode="411051 ";
+var gender="female ";
+var is_marrried="single ";
+console.log(firstName,lastName,myAge,address,pincode,gender,is_marrried)

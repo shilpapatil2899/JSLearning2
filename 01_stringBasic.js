2fbac888f333cd583a22b3e6ac577334ecdb3a77
Shilpa Patil <123926545+shilpapatil2899@users.noreply.github.com>
2023-02-28 15:44:32 +0530

    stringBasic.js
    stringBasic.js

---
+++ 01_stringBasic.js
@@ -0,0 +1,24 @@
+console.log(1);
+console.log(`My dream company is :- "IBM"`);
+ console.log("===========================");
+
+console.log(2);
+var hobby1="South Movie ";  //declaration
+console.log(`my hobbies are- ${hobby1}`);
+
+var hobby2="Larning New things ";  //declaration
+console.log(`my hobbies are- ${hobby2}`);
+
+var hobby3=" Swimming ";  //declaration
+console.log(`my hobbies are- ${hobby3}`);
+console.log("===========================");
+
+console.log(3);
+console.log("---------- concat()------------");
+var hobby1= " South Movie";
+var hobby2 = "Swimming";
+var hobby3 = "Larning New things";
+// Concatenation using + operator 
+var result = hobby1 + hobby2+ hobby3;
+var result = hobby1.concat(hobby2,hobby3);
+console.log("Concatenation using concat(): ", result);

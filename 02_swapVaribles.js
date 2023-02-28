4b156c95058e9939c577ecc34970dd29adb51917
Shilpa Patil <123926545+shilpapatil2899@users.noreply.github.com>
2023-02-28 14:55:39 +0530

    02_swapvariable.js
    02_swapvariable.js

---
+++ 02_swapVaribles.js
@@ -0,0 +1,12 @@
+var sweety="Sweety";
+var cutie="cutie"
+
+console.log("sweety=" +sweety , "cutie=" +cutie);
+
+console.log("After Swaping");
+
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+
+console.log("sweety=" +sweety , "cutie=" +cutie);
